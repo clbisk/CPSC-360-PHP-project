d37f2dc803def35fc62ffd404637e4e924fbad7a
Cecilia Bisk <clbisk@gmail.com>
2018-11-28 13:20:35 -0500

    testing

--- password.php
+++ password.php
@@ -1,1 +1,14 @@
-echo("Boo");
+<html><body>
+
+<?php
+$password = $_POST['password'];
+
+$db = new mysqli('localhost', 'wordsroot', $password, 'words');
+
+if ($db-> connect_errno) {
+	echo("Can't connect");
+} else {
+	echo("Hello");
+}
+
+</body></html>
